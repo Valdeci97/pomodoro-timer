0bfac25d719d2253e94815e07fb1f84fde756536
valdeci <moura97.valdeci@gmail.com>
2023-01-14 15:33:54 -0300

    feat(pages): create cronodoro

---
+++ app/src/components/Pomodoro.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Timer from './Timer.svelte';
+</script>
+
+<div class="app-container">
+  <Timer />
+</div>
+
+<style>
+  .app-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 90vh;
+  }
+</style>
---
+++ app/src/pages/Cronodoro.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  import Header from '../components/Header.svelte';
+  import Pomodoro from '../components/Pomodoro.svelte';
+  import { setupBackground } from '../utils/background';
+  import { POMODORO } from '../utils/constants';
+
+  onMount(() => {
+    setupBackground(POMODORO);
+  });
+</script>
+
+<main>
+  <Header />
+  <Pomodoro />
+</main>
+
+
